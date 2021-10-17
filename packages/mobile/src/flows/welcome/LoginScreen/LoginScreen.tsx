884a196a61a6c7a8497e9ac227c1c0af3247ad65
Brandon Phillips <brandon.phillips976@gmail.com>
2021-10-16 19:11:42 -0700

    commit to move to desktop

--- packages/mobile/src/flows/welcome/LoginScreen/LoginScreen.tsx
+++ packages/mobile/src/flows/welcome/LoginScreen/LoginScreen.tsx
@@ -1,9 +1,11 @@
 import React from "react"
-import { View } from "react-native"
+import { View, Text } from "react-native"
 import { RouteProp } from "@react-navigation/native"
 import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 import { RootStackParamList, screens } from "~Navigation/Navigation"
 import styles from "./LoginScreen.style"
+import TextInputField from "~Components/TextInputField/TextInputField";
+import { useState } from "hoist-non-react-statics/node_modules/@types/react"
 
 export interface LoginScreenParamList {
 
@@ -15,8 +17,13 @@ interface LoginScreenProps {
 }
 
 const LoginScreen = (props: LoginScreenProps) => {
+    const [formValues, onInputChange] = useState({ email: "", password: "" });
+
     return (
-        <View/>
+        <View>
+            <Text>Login</Text>
+            <TextInputField label={"Email"} placeholder={"Email"} onChangeText={(text) => onInputChange({ ...formValues, email: text })}/>
+        </View>
     )
 }
 
