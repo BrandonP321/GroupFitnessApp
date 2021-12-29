2744942af6908355e01038a542aacf381a5f1d8d
Brandon Phillips <brandon.phillips@bphillips.dev>
2021-12-28 20:10:17 -0800

    Added utility functions for getting and setting jwt's

--- packages/mobile/utils/JWTUtils.ts
+++ packages/mobile/utils/JWTUtils.ts
@@ -1,15 +1,22 @@
+import { storeAccessToken } from "../src/features/user/userSlice";
+import { store } from "../src/features/store";
+import { StorageUtils } from "./StorageUtils";
+
 export class JWTUtils {
-    public static getAccessTokenFromStorage() {
+    public static async getAccessToken() {
+        try {
+            const token = store.getState()?.user?.accessToken
 
+            return token
+        } catch (err) {
+            return undefined;
+        }
     }
 
-    /* securely stores access token in storage */
-    public static storeAccessToken() {
+    public static async getRefreshTokenFromStorage() {
+        const token = await StorageUtils.getSecureItemAsync("refreshToken");
 
-    }
-
-    public static getRefreshTokenFromStorage() {
-
+        return token;
     }
 
     /* securely stores refresh token in storage */
@@ -23,5 +30,28 @@ export class JWTUtils {
 
     private static getTokenFromStorage() {
 
+    }
+
+    /* stores both the access and refresh token */
+    public static async setBothTokens(accessToken: string, refreshToken: string) {
+        return new Promise<boolean>(async (resolve, reject) => {
+            try {
+                // store both tokens in async storage
+                const setAccessToken = await StorageUtils.setSecureItemAsync("accessToken", accessToken);
+                const setRefreshToken = await StorageUtils.setSecureItemAsync("refreshToken", refreshToken);
+    
+                if (!setAccessToken || !setRefreshToken) {
+                    return reject(false);
+                }
+        
+                // store access token in redux store
+                store.dispatch(storeAccessToken(accessToken))
+
+                return resolve(true);
+            } catch (err) {
+
+                return reject(false);
+            }
+        })
     }
 }
